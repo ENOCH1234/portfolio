a36a8a081e9fa709f0f7fd1c3efe3757fad9bf51
Olayode Enoch <olayodeenoch@gmail.com>
2021-12-08 22:33:03 +0100

    Changed mail file

--- mail.php
+++ mail.php
@@ -1,52 +1,20 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require_once "vendor/autoload.php";
-
-// Get data from form
 $fname = $_POST["fname"];
 $email = $_POST["email"];
 $text = $_POST["message"];
 $phone = $_POST["phone"];
 $sub = $_POST["subject"];
 
-//PHPMailer Object
-$mail = new PHPMailer(true); //Argument true in constructor enables exceptions
-
-//From email address and name
-$mail->From = "info@olayodeenoch.com";
-$mail->FromName = $fname;
-
-//To address and name
-// $mail->addAddress("recepient1@example.com", "Recepient Name");
-$mail->addAddress("olayodeenoch@gmail.com"); //Recipient name is optional
-
-//Address to which recipient will reply
-$mail->addReplyTo($email, "Reply");
-
-//CC and BCC
-// $mail->addCC("cc@example.com");
-// $mail->addBCC("bcc@example.com");
-
-//Send HTML or Plain Text email
-$mail->isHTML(true);
-
-// $mail->Subject = "Subject Text";
-$mail->Subject = $sub;
-$mail->Body =
+$to = "olayodeenoch@gmail.com";
+$subject = "My Website: " . $sub;
+$txt =
     "<table style='width:100%'>
         <tr><td>' . $fname . '</td></tr>
         <tr><td>Email: ' . $email . '</td></tr>
         <tr><td>phone: ' . $phone . '</td></tr>
         <tr><td>Message: ' . $text . '</td></tr>
     </table>";
-// $mail->AltBody = "This is the plain text version of the email content";
+$headers = "From: " . $email;
 
-try {
-    $mail->send();
-    echo "Message has been sent successfully";
-} catch (Exception $e) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-}
+mail($to, $subject, $txt, $headers);
